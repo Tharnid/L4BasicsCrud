e6ecee5e7945dfdc348129e6c9ecb7f39c1dac2e
Tharnid <jeffmsmithdev@gmail.com>
2013-10-21 23:41:37 -0400

    done with update

--- app/views/posts/edit.blade.php
+++ app/views/posts/edit.blade.php
@@ -1,1 +1,37 @@
-edit.blade
+@extends('layouts.master')
+
+@section('content')
+
+<h1>Edit Post</h1>
+
+{{ Form::model($post, array('method' => 'PATCH', 'route', => array('posts.update', $post->id))) }}
+<ul>
+	<li>
+		{{ Form::label('author', 'Author:') }}
+		{{ Form::text('author', '', array('placeholder' => 'Author')) }}
+	</li>
+	<br />
+	<li>
+		{{ Form::label('title', 'Title:') }}
+		{{ Form::text('title', '', array('placeholder' => 'Title')) }}
+	</li>
+	<br />
+	<li>
+		{{ Form::label('body', 'Body:') }}
+		{{ Form::textarea('body', '', array('placeholder' => 'Body')) }}
+	</li>
+	<br />
+	<li>
+		{{ Form::submit('Save Post', array('class' => 'btn-info')) }}
+		{{ link_to_route('posts.show', 'Cancel', $post->id, array('class' => 'btn btn-warning'))}}
+	</li>
+</ul>
+{{ Form::close() }}
+
+			@if($errors->any())
+				<ul>
+					{{ implode('', $errors->all('<li class="error">:message</li>'))}}
+				</ul>
+			@endif
+
+@stop
